67aa9eb9bea5730a1fb4912767232e176d346005
donkimber <donkimber@gmail.com>
2023-08-21 10:28:49 -0700

    cleanup and new features

--- Circles/Player.js
+++ Circles/Player.js
@@ -2,6 +2,11 @@
 //  Date:    Jan 1st. 2017
 "use strict";
 
+/*
+inspired by other Risset rhythm based music and visualization
+particularly: /* Inspired by other Risset rhythm based musics and visualizations, particularly
+https://youtu.be/6l2EtiEC0pU
+*/
 let synth = null;
 
 function initSynth() {
@@ -12,15 +17,17 @@ function initSynth() {
         document.getElementById("prog").appendChild(o);
     }
     //this.SetProgram(116);
+    /*
     setInterval(function () {
         var st = synth.getPlayStatus();
         document.getElementById("status").innerHTML =
             "Play:" + st.play + "  Pos:" + st.curTick + "/" + st.maxTick;
     }, 100);
+    */
     return synth;
 }
 
-function keyStart(key = 60, v=100) {
+function keyStart(key = 60, v = 100) {
     //console.log("keyStart", key, v);
     synth.noteOn(0, key, v);
 }
@@ -35,6 +42,7 @@ function getTime() {
     return new Date().getTime() / 1000;
 }
 
+// return log base 2 of x
 function log2(x) {
     return Math.log(x) / Math.log(2);
 }
@@ -84,6 +92,16 @@ class Note {
     }
 }
 
+let PATTERNS = {};
+PATTERNS['major'] = [0, 2, 4, 5, 7, 9, 11, 12];
+PATTERNS["chromatic"] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+PATTERNS["fixed"] = [0, 0, 0, 0, 0, 0, 0, 0];
+PATTERNS["quarters"] = [0, 0, 0, 0];
+PATTERNS["eighths"] = [0, 0, 0, 0, 0, 0, 0, 0];
+
+// this is one voice.  It corresponds to a circle, with dots on
+// the circle corresponding to notes.  The circle rotates at a
+// speed that is a power of 2 different from the reference speed.
 class Part {
     constructor(i, cx, cy) {
         console.log("Part(", i, ")");
@@ -94,20 +112,19 @@ class Part {
         this.cx = cx;
         this.cy = cy;
         this.vel = 100;
-        let major = [0, 2, 4, 5, 7, 9, 11, 12];
-        let chromatic = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-        let fixed = [0, 0, 0, 0, 0, 0, 0, 0];
-        let melody = fixed;
-        //melody = major;
-        //melody = chromatic;
-        this.setNotes(melody);
+        this.setNotes("fixed");
     }
 
-    setNotes(melody) {
+    setNotes(name) {
+        let pattern = PATTERNS[name];
+        if (pattern == null) {
+            alert("pattern " + name + " not found");
+            pattern = [0];
+        }
         this.notes = [];
-        let numSteps = melody.length;
+        let numSteps = pattern.length;
         for (let i = 0; i < numSteps; i++) {
-            this.notes.push(new Note(i / numSteps, MNOTE + melody[i]));
+            this.notes.push(new Note(i / numSteps, MNOTE + pattern[i]));
         }
     }
 
@@ -115,9 +132,14 @@ class Part {
     // from that speed.
     setSpeed(speed) {
         this.speed = speed;
-        //this.speed = s0 * Math.pow(2, -this.i);
         this.r = 100 - log2(this.speed) * 30;
-        this.setWeight();
+        // set some weights used for drawing
+        // and key velocity
+        let k = log2(speed);
+        let d = k + 1;
+        let w = 1 / (1 + 4 * d * d);
+        this.vel = Math.floor(60 * w);  // intensity of note
+        this.w = w;                     // visibility of circle
     }
 
     multSpeed(f) {
@@ -132,19 +154,11 @@ class Part {
         this.t = t;
     }
 
-    setWeight() {
-        let k = log2(this.speed);
-        let d = k + 1;
-        let w = 1/(1 + 4*d*d);
-        this.vel = Math.floor(60 * w);
-        this.w = w;
-    }
-
     dump() {
         console.log("Part(", this.i, ") t=", this.t,
-                "T=", this.T, "speed=", this.speed,
-                "r=", this.r, "vel=", this.vel,
-                "w=", this.w   );
+            "T=", this.T, "speed=", this.speed,
+            "r=", this.r, "vel=", this.vel,
+            "w=", this.w);
     }
 
     getNoteGraphic(i) {
@@ -159,7 +173,7 @@ class Part {
         let y = this.cy - this.r * Math.cos(th);
         let a = this.w;
         //let color = note.state == "on" ? "rgb(0, 255, 0)" : "gray";
-        let color = note.state == "on" ? `rgba(0, 255, 0, ${a})` :`rgba(100,100,100,${a})`;
+        let color = note.state == "on" ? `rgba(0, 255, 0, ${a})` : `rgba(100,100,100,${a})`;
         return { x, y, color }
     }
 }
@@ -181,15 +195,24 @@ class Player {
         this.parts = [];
         let numParts = 5;
         for (let i = 0; i < numParts; i++) {
-            let part = new Part(i-1, this.cx, this.cy);
+            let part = new Part(i - 1, this.cx, this.cy);
             part.setSpeed(Math.pow(2, -i));
             this.parts.push(part);
         }
+        this.reset();
+    }
+
+    setNotes(patternName) {
+        for (let i = 0; i < this.parts.length; i++) {
+            let part = this.parts[i];
+            part.setNotes(patternName);
+        }
+        this.reset();
     }
 
     handleAcc(val) {
         let v = (val - 50) / 50;
-        let f = Math.pow(2, 0.01*v);
+        let f = Math.pow(2, 0.01 * v);
         console.log("handleAcc", val, v, f);
         this.speedFactor = f;
     }
@@ -212,20 +235,32 @@ class Player {
         for (let i = 0; i < this.parts.length; i++) {
             this.parts[i].setT(0);
         }
+        this.draw();
+    }
+
+    togglePlay() {
+        if ($("#play").html() == "Play") {
+            this.start();
+        } else {
+            this.stop();
+        }
     }
 
     // start animation loop
     start() {
         // request animation frame
-        this.reset();
+        //this.reset();
+        $("#play").html("Stop");
         if (this.playing) {
             return;
         }
         this.playing = true;
+        this.prevT = getTime();
         window.requestAnimationFrame(this.update.bind(this));
     }
 
     stop() {
+        $("#play").html("Play");
         this.playing = false;
     }
 
@@ -243,29 +278,17 @@ class Player {
             this.speed = rampUpAndDown(ct - this.t0, 20, 1, 3);
         }
         //this.speed *= 0.999;
-        $("#speed").html("speed:" + this.speed.toFixed(2) + "f: " + this.speedFactor.toFixed(4));
+        let statStr = "speed:" + this.speed.toFixed(2) +
+                " f: " + this.speedFactor.toFixed(4) +
+                " playTime:" + this.playTime.toFixed(2);
+        $("#speed").html(statStr);
         //
         // check whether time to remove and add a player ring
         //
-        let k = Math.floor(log2(this.speed));
-        let nparts = this.parts.length;
-        let part = this.parts[nparts-1];
-        let lgs = log2(part.speed);
-        //console.log("lgs", lgs);
-        if (lgs < -3) {
-            console.log("lgs", lgs);
-            let part0 = this.parts[0];
-            let i = part0.i - 1;
-            let part = new Part(i, this.cx, this.cy);
-            part.setSpeed(part0.speed * 2);
-            part.setT(part0.t * 2);
-            this.parts.pop(); // get rid of hightest / fastest one
-            // insert part at beginning
-            this.parts.unshift(part);
-        }
+        this.checkForNewParts();
         //console.log("speed",  this.speed.toFixed(2), this.speed);
         let dpt = this.speed * dct; // dpt = delta play time
-        this.t += dpt;
+        this.playTime += dpt;
         // set t for each part
         for (let i = 0; i < this.parts.length; i++) {
             let part = this.parts[i];
@@ -279,8 +302,44 @@ class Player {
         window.requestAnimationFrame(this.update.bind(this));
     }
 
+    checkForNewParts() {
+        let nparts = this.parts.length;
+        if (this.speedFactor < 1) {
+            let part = this.parts[nparts - 1];
+            let lgs = log2(part.speed);
+            if (lgs < -3) {
+                console.log("decc lgs", lgs);
+                let part0 = this.parts[0];
+                let i = part0.i - 1;
+                let part = new Part(i, this.cx, this.cy);
+                part.setSpeed(part0.speed * 2);
+                part.setT(part0.t * 2);
+                this.parts.pop(); // get rid of hightest / fastest one
+                // insert part at beginning
+                this.parts.unshift(part);
+            }
+        }
+        if (this.speedFactor > 1) {
+            let part0 = this.parts[0];
+            let lgs = log2(part0.speed);
+            if (lgs > 1) {
+                console.log("acc lgs", lgs);
+                let part_ = this.parts[nparts - 1];
+                let i = part_.i + 1;
+                let part = new Part(i, this.cx, this.cy);
+                part.setSpeed(part_.speed / 2);
+                part.setT(part_.t / 2);
+                this.parts.shift(); // get rid of lowest / slowest one
+                // insert part at end
+                this.parts.push(part);
+            }
+        }
+    }
+
     // draw the animation frame
     draw(ctx) {
+        if (!ctx)
+            ctx = this.ctx;
         // clear the canvas
         ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         // draw each part
@@ -304,15 +363,15 @@ class Player {
         ctx.lineWidth = part.w;
         ctx.strokeStyle = '#003366';
         ctx.stroke();
-        let drawLabels = 1;
+        let drawLabels = $("#labels").prop("checked");
         if (drawLabels) {
             let lgs = log2(part.speed);
-            let str = "" + part.i+" "+part.vel+" "+part.w.toFixed(2);
-            str += " "+lgs.toFixed(2);
+            let str = "" + part.i + " " + part.vel + " " + part.w.toFixed(2);
+            str += " " + lgs.toFixed(2);
             // write str as text at x,y+part.r
             ctx.font = "14px Arial";
             ctx.fillStyle = "black";
-            ctx.fillText(str, x-5, y - (r + 5));
+            ctx.fillText(str, x - 5, y - (r + 5));
         }
         for (let i = 0; i < part.notes.length; i++) {
             let ng = part.getNoteGraphic(i);
@@ -325,7 +384,7 @@ class Player {
     }
 
     dump() {
-        for (let i=0; i<this.parts.length; i++) {
+        for (let i = 0; i < this.parts.length; i++) {
             let part = this.parts[i];
             part.dump();
         }
